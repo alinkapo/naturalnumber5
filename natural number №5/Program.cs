9a62b2448bcdfe0989afc9abe89b52c33e64e363
Андрей <@DESKTOP-USER>
2022-11-10 13:35:44 +0400

    Добавьте файлы проекта.

---
+++ natural number №5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace natural_number__5
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Введите любое натуральное число, состоящее из 5 цифр:");
+    var N=Convert.ToInt32(Console.ReadLine());
+    int M;
+    if (N >= 10_000 && N < 100_000)
+    {
+    Console.WriteLine("Новое число, которое образовалось путем исключения средней цифры:"); 
+    M=(N/1000)*100 + N%100;
+    }
+    else
+    {
+    Console.WriteLine("Число не состоит из 5 цифр. Будьте внимательнее!");
+    M = N;
+    }
+    Console.WriteLine(M);
+
+    }
+  }
+}
